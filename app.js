d35312d3adfa1c0cbcab569e34177d3f766a328b
Alexandra Colorado <alexandrapena19@gmail.com>
2023-08-22 20:58:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const form = document.querySelector("#form");
-const taskList = document.querySelector("#task-list");
+    const taskList = document.querySelector("#task-list");
+    const newToDO = document.querySelector("#newToDo");
+    const submitButton = document.querySelector("#submit-button");
 
-subitButton.addEventListener('click',function(e){
-    e.preventDefault();
-    const newToDoInput = newToDo.value
-    if(newToDoInput !== ''){
-        const createLi = document.createElement('li');
-        createLi.textContent = newToDoInput;
-        taskList.appendChild(createLi);
-        newToDoInput.value = '';
-    }
-});
+    submitButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        const newToDoInput = newToDO.value;
+        if (newToDoInput.trim() !== '') {
+            const createLi = document.createElement('li');
+            const toDoText = document.createElement('span');
+            toDoText.textContent = newToDoInput;
 
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'Remove';
+            removeButton.classList.add('remove-button');
+            removeButton.addEventListener('click', function() {
+                createLi.remove();
+            });
+
+            createLi.appendChild(toDoText);
+            createLi.appendChild(removeButton);
+
+            taskList.appendChild(createLi);
+            newToDO.value = '';
+        }
+    });
+
+    taskList.addEventListener('click', function(e) {
+        if (e.target.classList.contains('remove-button')) {
+            const listItem = e.target.closest('li');
+            listItem.remove();
+        } else if (e.target.tagName === 'LI') {
+            e.target.classList.toggle('completed');
+        }
+    });
 
 
